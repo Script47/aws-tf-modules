6393bb24b80fce5ccb882a8e1a7c878bf5ffd4c9
Script47 <Script47@hotmail.com>
2024-12-01 23:42:43 +0000

    Bug fix for ACM records

--- static-site/acm.tf
+++ static-site/acm.tf
@@ -5,7 +5,7 @@ resource "aws_acm_certificate" "cloudfront_cert" {
   provider          = aws.acm
 }
 
-resource "aws_acm_certificate_validation" "cert_validation" {
+resource "aws_acm_certificate_validation" "cloudfront_cert_validation" {
   certificate_arn         = aws_acm_certificate.cloudfront_cert.arn
   validation_record_fqdns = [for record in aws_route53_record.acm_records : record.fqdn]
 }
