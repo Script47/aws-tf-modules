6b2ceb8bf1ec0e74e3661e549e44be0a3b1faf6c
Script47 <Script47@hotmail.com>
2024-12-01 23:32:49 +0000

    Bug fix for acm_records ref

--- static-site/route53.tf
+++ static-site/route53.tf
@@ -4,7 +4,7 @@
 
 resource "aws_route53_record" "acm_records" {
   for_each = {
-    for dvo in aws_acm_certificate.aws_acm_certificate.cloudfront_cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cloudfront_cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
