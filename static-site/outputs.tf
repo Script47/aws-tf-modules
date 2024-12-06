1b0e92e7437bac17d4500edbe70f757fa864241d
Script47 <Script47@hotmail.com>
2024-12-06 15:34:08 +0000

    feat(static-site)!: change output variable names

--- static-site/outputs.tf
+++ static-site/outputs.tf
@@ -6,10 +6,10 @@ output "deploy_role_arn" {
   value = aws_iam_role.deploy_static_site.arn
 }
 
-output "cloudfront_dist_id" {
+output "cloudfront_id" {
   value = aws_cloudfront_distribution.static_site.id
 }
 
-output "cloudfront_dist_domain_name" {
+output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.static_site.domain_name
 }
