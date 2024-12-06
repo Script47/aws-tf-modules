26f47f8b0bae21b911ec4b880309da2b03f303b6
Script47 <Script47@hotmail.com>
2024-12-06 23:57:42 +0000

    feat(static-site)!: refactor cloudfront output variables

--- static-site/outputs.tf
+++ static-site/outputs.tf
@@ -6,10 +6,10 @@ output "deploy_role_arn" {
   value = aws_iam_role.deploy_static_site.arn
 }
 
-output "cloudfront_dist_id" {
-  value = aws_cloudfront_distribution.static_site.id
-}
-
-output "cloudfront_dist_domain_name" {
-  value = aws_cloudfront_distribution.static_site.domain_name
+output "cloudfront" {
+  value = {
+    id          = aws_cloudfront_distribution.static_site.id
+    domain_name = aws_cloudfront_distribution.static_site.domain_name
+    aliases     = aws_cloudfront_distribution.static_site.aliases
+  }
 }
