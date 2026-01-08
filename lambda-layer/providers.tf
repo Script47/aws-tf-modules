470d03d22d604ada6d66c992e4a2cfaa5a3cca96
Script47 <Script47@hotmail.com>
2026-01-08 21:52:00 +0000

    add missing provider

--- lambda-layer/providers.tf
+++ lambda-layer/providers.tf
@@ -7,6 +7,11 @@ terraform {
       version = "~> 6"
     }
 
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 3.0.0, < 4.0.0"
+    }    
+
     archive = {
       source  = "hashicorp/archive"
       version = ">= 2.0.0, < 3.0.0"
