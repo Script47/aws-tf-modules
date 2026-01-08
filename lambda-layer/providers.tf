2cc8ded19856cca0ff3871cbecb3169f58b186b5
Script47 <Script47@hotmail.com>
2026-01-08 21:50:19 +0000

    required_version

--- lambda-layer/providers.tf
+++ lambda-layer/providers.tf
@@ -1,8 +1,15 @@
 terraform {
+  required_version = ">= 1.13"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 6"
+    }
+
+    archive = {
+      source  = "hashicorp/archive"
+      version = ">= 2.0.0, < 3.0.0"
     }
   }
 }
